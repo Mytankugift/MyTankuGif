3c4c9a1977f846295dde23e41c07cb359cb752eb
devRonaldo <ronalldocorrea@gmail.com>
2025-05-02 09:41:57 -0500

    integracion del servicio de s3

--- Tanku/medusa-config.ts
+++ Tanku/medusa-config.ts
@@ -28,14 +28,20 @@ module.exports = defineConfig({
       options: {
         providers: [
           {
-            resolve: "@medusajs/medusa/file-local",
-            id: "local",
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
             options: {
-              upload_dir: "static",
-              backend_url: process.env.MEDUSA_BACKEND_URL,
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              // other options...
             },
           },
         ],
+
       },
     },
   ],
