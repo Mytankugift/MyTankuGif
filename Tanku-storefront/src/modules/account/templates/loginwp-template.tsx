e425ffcc2b8d2a025b47011efc9efc9dfaaeaf77
devRonaldo <ronalldocorrea@gmail.com>
2025-05-23 02:42:38 -0500

    ajuste

--- Tanku-storefront/src/modules/account/templates/loginwp-template.tsx
+++ Tanku-storefront/src/modules/account/templates/loginwp-template.tsx
@@ -9,24 +9,15 @@ type DecodedToken = {
 }
 export const LoginWPTemplate = ({ token }: { token: string }) => {
 
-  const authenticateWordpress = async (token: string) => {
-    try {
-      // Use the server action to verify the token instead of doing it client-side
-      const decoded = await verifyWordpressToken(token)
-      console.log("datos del token:", decoded)
+      verifyWordpressToken(token).then((decoded) => {
+        console.log("datos del token:", decoded)
 
-      if (decoded && decoded.eml) {
-        await loginWordpress(decoded.eml, token)
-      } else {
-        console.error("Token verification failed: Invalid token data")
-      }
-    } catch (error) {
-      console.error("Token verification failed:", error)
-    }
-  }
-  useEffect(() => {
-    authenticateWordpress(token)
-  }, [token])
+        if (decoded && decoded.eml) {
+          loginWordpress(decoded.eml, token)
+        } else {
+          console.error("Token verification failed: Invalid token data")
+        }
+      })
 
   return <div className="">token {token}</div>
 }
